d7b788994b97b57b853a0474bf6879630c4ea420
sumit sharma <sumitsharmavoice@gmail.com>
2026-04-02 10:30:54 +0530

    changed live backend

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://taskmanagerbackend-production-cac7.up.railway.app",
+  // baseURL: process.env.NEXT_PUBLIC_API_URL || "https://taskmanagerbackend-production-cac7.up.railway.app",
+  baseURL: "https://task-manager-backend-ixgx.onrender.com" ,
   headers: { "Content-Type": "application/json" },
 });
 
